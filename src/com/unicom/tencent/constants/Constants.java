6b7684a511fd58c495b2d58b19b61d24693890e7
BearnLee <136026191@qq.com>
2018-07-23 17:11:18 +0800

    安全性保证

--- src/com/unicom/tencent/constants/Constants.java
+++ src/com/unicom/tencent/constants/Constants.java
@@ -1,9 +1,10 @@
 package com.unicom.tencent.constants;
 
 public class Constants {
-	public static final String SECRET_ID = "AKIDgEdw5AFy0NlAgEniXDvU2UpAJNH23BWn";
-	public static final String SECRET_KEY= "Rnz0qZFrIYq6B5oOsDtbCczGQUQBk8OS";
+	public static final String SECRET_ID = "您的账号SecretId";
+	public static final String SECRET_KEY= "您的账号SECRET_KEY";
 	
-	public static final String TENCENT_CLOUD_SECRET_ID = "AKIDBGAwYzcNLAnHajmRCpKgqocFg1FREmid";
-	public static final String TENCENT_CLOUD_SECRET_KEY= "zoRvIUVL0hF5rSs7C5nJeomcHoCOQJ2Z";
+	//用于审批
+	public static final String TENCENT_CLOUD_SECRET_ID = "云商的账号SecretId";
+	public static final String TENCENT_CLOUD_SECRET_KEY= "云商的账号SECRET_KEY";
 }
